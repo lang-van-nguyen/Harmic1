8ba5ecb2dae2b3fd7b75fa876aa63de93590dff5
lang-van-nguyen <langnguyen958@gmail.com>
2024-12-13 12:17:42 +0700

    th11

---
+++ Areas/Admin/Controllers/FileManagerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Harmic1.Areas.Admin.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        [Area("Admin")]
+        [Route("/Admin/FileManager/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ContactController.cs
@@ -0,0 +1,41 @@
+﻿using Harmic1.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Harmic1.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly DataTh2Context _context;
+        public ContactController(DataTh2Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string name, string phone, string email, string message)
+        {
+            try
+            {
+                TbContact contact = new TbContact();
+                contact.Name = name;
+                contact.Phone = phone;
+                contact.Email = email;
+                contact.Message = message;
+                contact.CreatedDate = DateTime.Now;
+                _context.Add(contact);
+                _context.SaveChangesAsync();
+
+                return Json(new { status = true });
+
+            }
+            catch
+            {
+                return Json(new { status = false });
+            }
+        }
+    }
+}
